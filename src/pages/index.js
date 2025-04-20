1487a277ce1e50f98de3655aa4ce4f2e82031480
Akshay Karande <akarande777@outlook.com>
2025-04-20 21:09:07 +0530

    Fix build error

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Box, Container, Typography } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import Features from '../components/features';
 
 export default function HomePage() {
   return (
-    <Container sx={{p: 0 }}>
+    <Container sx={{ p: 0 }}>
       <Typography variant="h4" align="center" my={3}>
         Visualization of Algorithms
       </Typography>
@@ -15,10 +15,11 @@ export default function HomePage() {
         From sorting and searching to more advanced data structures and
         algorithms, <strong>see algorithms</strong> provides a hands-on approach
         to learning. Each animation is carefully crafted to walk you through the
-        inner workings of various algorithms, step by step. Whether you're a
-        student seeking to solidify your knowledge, an educator looking for
+        inner workings of various algorithms, step by step. Whether you&apos;re
+        a student seeking to solidify your knowledge, an educator looking for
         dynamic teaching tools, or simply someone with a passion for computer
-        science, you'll find value in our extensive library of visual resources.
+        science, you&apos;ll find value in our extensive library of visual
+        resources.
       </Typography>
       <Features />
     </Container>
